86bf03bb23ed77e9bdd9799d8fbf799510a24608
MajaJ <92257962+majajuric@users.noreply.github.com>
2022-12-29 14:01:59 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-# setup.py
-
-from distutils.core import setup, Extension
-
-ssk_module = Extension('_ssk', sources=['ssk.c', 'ssk.i'])
-
-setup(name='ssk', ext_modules=[ssk_module], py_modules=["ssk"])
